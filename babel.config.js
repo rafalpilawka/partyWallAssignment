744cabb2d9fc448c0a8efe54a5b3dd374838cd81
rafalpilawka <rafalpilawka@gmail.com>
2020-10-16 19:23:58 +0200

    initial commit with absolute

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            assets: "./assets",
+            components: "./src/components",
+            services: "./src/services",
+            store: "./store",
+            types: "./src/types",
+            constants: "./src/constants"
+          }
+        }
+      ]
+    ]
   };
 };
